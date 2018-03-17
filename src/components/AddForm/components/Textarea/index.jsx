7bcbe4005ede479d7cf3874e18fe782f2517359f
Bo Skov Selbach <boss@Bos-MacBook-Pro.lan>
2018-03-17 15:31:15 +0100

    Adds proptypes and default props for Textarea component

--- src/components/AddForm/components/Textarea/index.jsx
+++ src/components/AddForm/components/Textarea/index.jsx
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 import {} from './style.scss';
 
 class Textarea extends PureComponent {
+  static propTypes = {
+    name: PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    name: ''
+  }  
   handleChange = (event) => {
     this.props.change(event);
   }
 
   render() {
     const { name } = this.props;
+    
     return (
     <div className="textarea">
       <label htmlFor={name}>{name}</label>
